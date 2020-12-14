e450cd0b7beeb396df2fd6e11096b9adff271eeb
Danny Wandeler <danny.wandeler15@gmail.com>
2020-12-14 16:07:42 -0700

    More commands
    Added more commands. Updated README to suggest using $help command instead of others.

--- DaosBot.py
+++ DaosBot.py
@@ -20,6 +20,10 @@ async def on_message(message):
     
     if message.content.startswith('$test'):
         await message.channel.send(test)
+
+    if message.content.startswith('$help'):
+        x = 'Printing the list of all available commands.'
+        await message.channel.send(test)
     
     if message.content.startswith('$cardshuffle'):
         x = shuffledDeck()
@@ -35,6 +39,14 @@ async def on_message(message):
 
     if message.content.startswith('$music'):
         x = 'Preparing youtube playback to your voice channel!'
+        await message.channel.send(x)
+
+    if message.content.startswith('$cashout'):
+        x = 'Cashing out of this game... Your total winnings were: '
+        await message.channel.send(x)
+
+    if message.content.startswith('$overview'):
+        x = 'Displaying an overview of the winnings of the last game...'
         await message.channel.send(x)
 
 client.run('Bot_Token')
